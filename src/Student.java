d12cd8f96a7feea03715fe0095f886613739e78e
01-09 <2843106425@qq.com>
2021-01-10 17:40:42 +0800

    构造方法

--- src/Student.java
+++ src/Student.java
@@ -6,7 +6,18 @@ public class Student extends User {
     QQ
      */
 
-    private  String zhuangye;
+   private  String zhuangye
     private  String QQ;
 
+    public String Student() {
+
+    public String getZhuangye(){return zhuangye;}
+    public void  setZhuangye(String zhuangye){
+        this.zhuangye=zhuangye;
+
+    }
+    public  String getQQ(){return QQ;}
+    public  void  setQQ(String QQ){
+        this.QQ=QQ;
+    }
 }
